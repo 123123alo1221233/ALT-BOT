649e95caa152d99d2f8c255a71840fcc2ab4c320
123123alo1221233 <34815600+123123alo1221233@users.noreply.github.com>
2018-01-18 22:43:40 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Eris = require("eris");
+
+var bot = new Eris("Mzk0MzYzODk0MzgwMTAxNjMy.DUL4wQ.1T1lIASoAhwgPwPphQtOYE3XyxY
+");
+// Replace BOT_TOKEN with your bot account's token
+
+bot.on("ready", () => { // When the bot is ready
+    console.log("Ready!"); // Log "Ready!"
+});
+
+bot.on("messageCreate", (msg) => { // When a message is created
+    if(msg.content === "!ping") { // If the message content is "!ping"
+        bot.createMessage(msg.channel.id, "Pong!");
+        // Send a message in the same channel with "Pong!"
+    } else if(msg.content === "!pong") { // Otherwise, if the message is "!pong"
+        bot.createMessage(msg.channel.id, "Ping!");
+        // Respond with "Ping!"
+    }
+});
+
+bot.connect(); // Get the bot to connect to Discord
